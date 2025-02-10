8a36725da99c25f34f51725b1fd71e365948dfda
AndyTonoTipo2 <andres.rojas1816@alumnos.udg.mx>
2025-02-10 14:24:33 -0600

    p7 sql

--- thelab.py
+++ thelab.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for,request,redirect,flash
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 thelabApp = Flask(__name__)
 
@@ -10,8 +13,17 @@ def home():
 def signin():
     return render_template('signin.html')    
 
-@thelabApp.route('/signup')
+@thelabApp.route('/signup',mothods=['POST','GET'])
 def signup():
+    if request.method == 'POST':
+        nombre =    request.form['nombre']
+        correo =    request.form['correo']
+        clave =    request.form['clave']
+        claveCifrada = generate_password_hash(clave)
+        telefono =    request.form['telefono']
+        fechareg = datetime.now()
+        
+
     return render_template('signup.html')    
 
 
