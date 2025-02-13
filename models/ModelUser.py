19714cc80c0d0b7e2df0c8c435e9ed9014afba54
kaatmii <katia.miranda1890@alumnos.udg.mx>
2025-02-13 17:38:47 -0600

    p9 ModelUser

---
+++ models/ModelUser.py
@@ -0,0 +1,28 @@
+from models.entities.User import User
+
+class ModelUser:
+    @classmethod
+    def signin(self,db,usuario):
+        try:
+            SelUsuario = db.connection.cursor()
+            SelUsuario.execute("SELECT * FROM usuario WHERE correo = %s",(usuario.correo,))
+            u = SelUsuario.fetchone()
+            if u is not None:
+                return User(u[0],u[1],u[2],User.validarClave(u[3],usuario.clave),u[4],u[5],u[6])
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
+
+    @classmethod
+    def get_by_id(self,db,id):
+        try:
+            SelUsuario = db.connection.cursor()
+            SelUsuario.execute("SELECT * FROM id WHERE id = %s",(id,))
+            u = SelUsuario.fetchone()
+            if u is not None:
+                return User(u[0],u[1],u[2],u[3],u[4],u[5],u[6])
+            else:
+                return None
+        except Exception as ex:
+            raise Exception(ex)
