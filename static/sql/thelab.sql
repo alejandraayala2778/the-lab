c35ede7071e2f327861c5e6e1721126aea10178c
kaatmii <katia.miranda1890@alumnos.udg.mx>
2025-03-14 14:59:03 -0600

    p18 datebase

--- static/sql/thelab.sql
+++ static/sql/thelab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-02-2025 a las 01:18:52
+-- Tiempo de generación: 14-03-2025 a las 20:35:13
 -- Versión del servidor: 8.0.41
 -- Versión de PHP: 7.4.9
 
@@ -20,6 +20,74 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `thelab`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carritos`
+--
+
+CREATE TABLE `carritos` (
+  `id` int NOT NULL,
+  `usuario_id` int NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles_carrito`
+--
+
+CREATE TABLE `detalles_carrito` (
+  `id` int NOT NULL,
+  `carrito_id` int NOT NULL,
+  `productos_id` int NOT NULL,
+  `cantidades` int NOT NULL,
+  `precio` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles_pedidos`
+--
+
+CREATE TABLE `detalles_pedidos` (
+  `id` int NOT NULL,
+  `pedido_id` int NOT NULL,
+  `productos_id` int NOT NULL,
+  `cantidades` int NOT NULL,
+  `precio` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `usuario` int NOT NULL,
+  `id` int NOT NULL,
+  `usuario_id` text NOT NULL,
+  `feha` datetime NOT NULL,
+  `total` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int NOT NULL,
+  `nombre` text NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio` datetime NOT NULL,
+  `stock` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -42,11 +110,61 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `correo`, `clave`, `telefono`, `fechareg`, `perfil`) VALUES
-(1, 'alejandra ayala vizcaino', 'alejandra.ayala2778@alumnos.udg.mx', 'scrypt:32768:8:1$rIZLJLzPmUdA9QYT$a76c3af2d74d7dd083552fb4d3a805b21ed3aad2a561cba19de9b65eaeb8eb9735d739f6e51f426d0eeadf9368e3ebd04718526277f1f1574d94a2a4d866b5c1', '3339036994', '2025-02-12 19:12:42', 'C');
+(1, 'alejandra ayala vizcaino', 'alejandra.ayala2778@alumnos.udg.mx', 'scrypt:32768:8:1$rIZLJLzPmUdA9QYT$a76c3af2d74d7dd083552fb4d3a805b21ed3aad2a561cba19de9b65eaeb8eb9735d739f6e51f426d0eeadf9368e3ebd04718526277f1f1574d94a2a4d866b5c1', '3339036994', '2025-02-12 19:12:42', 'A'),
+(2, 'roberto cantor', 'roberto.cantor2757@alumnos.udg.mx', 'scrypt:32768:8:1$VnLWrbNsCyhg4cqR$53048914920d4dc3866165b8dd8eca5a1b1a630bde3620badd0db425de26782a35e5a633342c61f391020f4af7b0038675a282e4f231015df26123fcf7dd7c71', '3339987457', '2025-02-18 15:48:49', 'C');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL,
+  `nombre` text NOT NULL,
+  `correo` text NOT NULL,
+  `contraseña` text NOT NULL,
+  `perfil` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `carritos`
+--
+ALTER TABLE `carritos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `detalles_carrito`
+--
+ALTER TABLE `detalles_carrito`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `carrito_id` (`carrito_id`),
+  ADD KEY `productos_id` (`productos_id`);
+
+--
+-- Indices de la tabla `detalles_pedidos`
+--
+ALTER TABLE `detalles_pedidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pedido_id` (`pedido_id`),
+  ADD KEY `productos_id` (`productos_id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuario`
@@ -56,6 +174,12 @@ ALTER TABLE `usuario`
   ADD UNIQUE KEY `correo` (`correo`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -63,7 +187,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
